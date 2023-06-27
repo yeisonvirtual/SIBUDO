fecb11e6b9954ad093c45373fe16ef973bb4ef52
yeisonvirtual <yeisonjr98@gmail.com>
2023-06-26 22:41:58 -0400

    fix index

--- SIBUDO/SIBUDO_app/views.py
+++ SIBUDO/SIBUDO_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "SIBUDO_app/home.html")
+    return render(request, "SIBUDO_app/index.html")
