710bb88abfef596698eb8f50d20e0fdbf311478a
eulises <eulisesbrazon@gmail.com>
2023-06-30 00:12:28 -0400

    feat authentication

--- SIBUDO/SIBUDO_app/urls.py
+++ SIBUDO/SIBUDO_app/urls.py
@@ -21,5 +21,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Index"),
-    path('registrar/', views.register_user, name="register_form"),
+    path('registrar_2/', views.register_user, name="register_form"),
 ]
--- SIBUDO/authentication/urls.py
+++ SIBUDO/authentication/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for SIBUDO project.
+URL configuration for proyecto_django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import register_user, sign_off, sing_in
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('SIBUDO_app.urls')),
-    path('gestion/', include('gestion_recursos.urls')),
-    path('authentication/', include('authentication.urls')),
+    path('', register_user.as_view(), name="authentication"),
+    path('logout', sign_off, name="logout"),
+    path('sing_in', sing_in, name="sing_in"),
 ]
