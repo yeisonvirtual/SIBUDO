cbcb26895f0b7327602e15732df926f2eacb9f0f
Luis Marquez <englamc@gmail.com>
2023-07-13 13:01:36 -0400

    Style - Adding Sub-views - Ready to Go XD

--- SIBUDO/gestion_prestamos/urls.py
+++ SIBUDO/gestion_prestamos/urls.py
@@ -22,5 +22,10 @@ from gestion_prestamos import views
 urlpatterns = [
     path('', views.prestamos, name="Prestamos"),
     path('gestion_prestamos/', views.gestion_prestamos, name="Gestion_Prestamos"),
+    path('generar_prestamo/', views.generar_prestamo, name="Generar_Prestamo"),
+    path('recibir_prestamo/', views.recibir_prestamo, name="Recibir_Prestamo"),
+    path('buscar_prestamo/', views.buscar_prestamo, name="Buscar_Prestamo"),
     path('gestion_sanciones/', views.gestion_sanciones, name="Gestion_Sanciones"),
+    path('generar_sancion/', views.generar_sancion, name="Generar_Sancion"),
+    path('visualizar_sanciones/', views.visualizar_sanciones, name="Visualizar_Sanciones"),
 ]
