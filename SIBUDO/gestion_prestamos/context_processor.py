1d79b5efcbb32aa55dbccb8c5ee946c50780be19
yeisonvirtual <yeisonjr98@gmail.com>
2023-07-26 17:15:52 -0400

    aux_recursos

--- SIBUDO/gestion_prestamos/context_processor.py
+++
@@ -1,5 +0,0 @@
-from .estudiante import Estudiante
-
-def estudiante_candidato(request):
-    estudiante = Estudiante()
-    return {'estudiante':estudiante}
--- SIBUDO/sanciones_estudiante/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def ver_sanciones_estudiante(request):
-    return render(request, 'sanciones_estudiante/ver_sanciones_estudiante.html', {})
