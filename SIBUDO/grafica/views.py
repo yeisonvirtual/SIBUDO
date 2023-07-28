76a05ea9db77145e50253b4d13e35a3878abc9b3
eulises <eulisesbrazon@gmail.com>
2023-07-28 11:19:38 -0400

    add bar chart

--- SIBUDO/grafica/views.py
+++ SIBUDO/grafica/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import Group
+from gestion_recursos.models import cantidad_libro, cantidad_trabajo
+from gestion_prestamos.models import Recurso_Disponible
+from django.db.models import Sum
 
 def grafica(request):
     rol_aviable = ['Estudiante', 'Bibliotecario', 'Invitado']
@@ -12,9 +15,18 @@ def grafica(request):
         series.append((num_users_in_group))
         labels.append((rol))
 
+    n_libros = obtener_cantidad_total_libros()
+    n_trabajos = obtener_cantidad_total_trabajos()
+    n_disponibles = obtener_cantidad_total_recursos_disponibles()
+    n_prestados = (n_libros + n_trabajos) - n_disponibles
+
     context = {
         'Pie_Chart_series': series,
         'Pie_Chart_labels': labels,
+        'n_libros': n_libros,
+        'n_trabajos': n_trabajos,
+        'n_disponible': n_disponibles,
+        'n_prestados': n_prestados,
     }
 
     return render(request, 'grafica/grafica.html', context)
@@ -33,5 +45,33 @@ def contar_grupo(group_name):
     return num_users_in_group
 
 
+def obtener_cantidad_total_libros():
+    try:
+        cantidad_total = cantidad_libro.objects.aggregate(total_libros=Sum('cantidad'))['total_libros']
+        if cantidad_total is None:
+            cantidad_total = 0
+    except cantidad_libro.DoesNotExist:
+        cantidad_total = 0
 
+    return cantidad_total
+
+def obtener_cantidad_total_trabajos():
+    try:
+        cantidad_total = cantidad_trabajo.objects.aggregate(total_trabajos=Sum('cantidad'))['total_trabajos']
+        if cantidad_total is None:
+            cantidad_total = 0
+    except cantidad_trabajo.DoesNotExist:
+        cantidad_total = 0
+
+    return cantidad_total
+
+def obtener_cantidad_total_recursos_disponibles():
+    try:
+        cantidad_total = Recurso_Disponible.objects.aggregate(total_recursos=Sum('n_disponibles'))['total_recursos']
+        if cantidad_total is None:
+            cantidad_total = 0
+    except Recurso_Disponible.DoesNotExist:
+        cantidad_total = 0
+
+    return cantidad_total
 
